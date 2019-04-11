3fe6de763d23b726f46dbd03383696fc9b9c2aa4
Kumar <sudhanshu.b.kumar@capgemini.com>
2019-04-11 17:57:33 +0530

    caseStudy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { AppComponent } from './app.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ListUserComponent},
+  {path:'list-user',component:ListUserComponent},
+  {path:'add-user',component:AddUserComponent},
+  {path:'edit-user',component:EditUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'casestudy'`, () => {
+  it(`should have as title 'casestudy1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('casestudy');
+    expect(app.title).toEqual('casestudy1');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to casestudy!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to casestudy1!');
   });
 });
